8c8fe30dea48873520cf35472de61e1a39ed096c
Shantanu <shantanu.algo@gmail.com>
2022-08-22 01:25:50 +0530

    card search

--- modules/qi-core/src/main/java/org/jpos/qi/views/demo/CardSearchView.java
+++ modules/qi-core/src/main/java/org/jpos/qi/views/demo/CardSearchView.java
@@ -11,6 +11,7 @@ import org.jpos.qi.services.SearchManager;
 
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -24,7 +25,8 @@ public class CardSearchView extends Composite<VerticalLayout> {
 	private TextField schemeField;
 	private TextField cardHolderRealIdField;
 	private Button searchButton;
-	private HorizontalLayout responseHL;
+	private VerticalLayout responseVL;
+	private HorizontalLayout responseDataHL;
 	private QI app;
 	private Label cardNumberResponse;
 	public CardSearchView() {
@@ -49,6 +51,7 @@ public class CardSearchView extends Composite<VerticalLayout> {
 		cardHolderRealIdField = new TextField();
 		cardHolderRealIdField.setPlaceholder("Real Id");
 		searchButton = new Button("Search");
+		searchButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 		searchButton.addClickListener(e-> {
 			searchCard();
 		});
@@ -57,31 +60,39 @@ public class CardSearchView extends Composite<VerticalLayout> {
 		hl1.setWidthFull();
 		hl1.add(tokenField,cardProductField,schemeField,cardHolderRealIdField,searchButton);
 
-		responseHL = new HorizontalLayout();
-		responseHL.setWidthFull();
-		responseHL.getStyle().set("border-top", "1px solid gray");
-		responseHL.setVisible(false);
-		VerticalLayout responseLabelVL = new VerticalLayout(new Label("Response"));
+		responseVL = new VerticalLayout();
+		responseVL.setWidthFull();
+		responseVL.getStyle().set("border-top", "2px solid #d8d8d8");
+		responseVL.setVisible(false);
+		
+		Label responseLabel = new Label("Response");
+		responseLabel.getStyle().set("font-weight", "500");
+		HorizontalLayout responseLabelHL = new HorizontalLayout(responseLabel);
+		responseLabelHL.setWidthFull();
+		VerticalLayout responseLabelVL = new VerticalLayout();
 		responseLabelVL.setWidthFull();
-		VerticalLayout responseDataVL = new VerticalLayout();
-		responseDataVL.setWidthFull();
-		cardNumberResponse = new Label();
-		responseDataVL.add(new Label("Card Number:"), cardNumberResponse);
-		responseHL.add(responseLabelVL, responseDataVL);
+		responseLabelVL.add(responseLabelHL); 
+		
+		responseDataHL = new HorizontalLayout();
+		responseDataHL.setWidthFull();
+		//cardNumberResponse = new Label();
+		//responseDataVL.add(new Label("Card Number:"), cardNumberResponse);
+		responseVL.add(responseLabelHL, responseDataHL);
 
-		vl.add(hl1,responseHL);
+		vl.add(hl1,responseVL);
 		return vl;	
 	}
 	private void searchCard() {
-		responseHL.setVisible(true);
+		responseDataHL.removeAll();
+		responseVL.setVisible(true);
 		Card card = getRandomCard();
-		cardNumberResponse.removeAll();
+		
 		String cardNumber = "";
 		if(card!=null) {
 			cardNumber = card.getBin()==null?"":card.getBin();
 			cardNumber = cardNumber+" "+card.getLastFour()==null?"":card.getLastFour();
 		}
-		cardNumberResponse.add(cardNumber);
+		responseDataHL.add(new Label("Card Number:"), new Label(cardNumber), new Label("Expiry Date:"), new Label(card.getEndDate().toString()));
 	}
 	
 	private Card getRandomCard() {
